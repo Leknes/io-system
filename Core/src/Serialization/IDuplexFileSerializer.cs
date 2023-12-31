ceef4d68e15ae560b252cd78b525b03a7a395397
Leknes <leonhardsenkel@gmail.com>
2023-12-31 13:12:50 +0100

    Cleaned up a few things

---
+++ Core/src/Serialization/IDuplexFileSerializer.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Runtime.CompilerServices;
+
+namespace Senkel.IO.Serialization;
+
+/// <summary>
+/// Represents a file serializer that is capable of both serialization and deserialization.
+/// </summary>
+public interface IDuplexFileSerializer : IFileDeserializer, IFileSerializer { }
+
